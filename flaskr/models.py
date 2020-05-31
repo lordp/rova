0e0511bc4bfcd74abc9a66a94d0eb37616de25b1
Darryl Hamilton <darryl@addict.net.nz>
2020-05-31 12:37:54 +1200

    dos2unix plus adding song details

--- flaskr/models.py
+++ flaskr/models.py
@@ -1,11 +1,28 @@
 from flaskr import db
+from sqlalchemy.orm import relationship
 
 
 class Played(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), nullable=False)
+    song_slug = db.Column(db.String(250), nullable=False, index=True)
     artist = db.Column(db.String(250), nullable=False)
-    artist_slug = db.Column(db.String(250), nullable=False)
-    station = db.Column(db.String(250), nullable=False)
+    artist_slug = db.Column(db.String(250), nullable=False, index=True)
+    station = db.Column(db.String(250), nullable=False, index=True)
     played_time = db.Column(db.DateTime)
     length = db.Column(db.Integer)
+
+
+# class Artist(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(250), nullable=False)
+#     slug = db.Column(db.String(250), nullable=False)
+#     songs = relationship("Song")
+
+
+# class Song(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'))
+#     name = db.Column(db.String(250), nullable=False)
+#     slug = db.Column(db.String(250), nullable=False)
+
--- flaskr/routes.py
+++ flaskr/routes.py
@@ -2,9 +2,17 @@ from flaskr import app, db
 from flaskr.models import Played
 from flaskr.util import parse_time
 
-from flask import render_template, request, url_for
+from flask import render_template, request, url_for, Response
 
 import math
+import json
+import re
+from datetime import datetime
+
+
+def find_start_date():
+    start_date = db.session.query(Played.played_time).order_by(Played.played_time)[0]
+    return (start_date[0], datetime.now().replace(microsecond=0))
 
 
 @app.route("/")
@@ -13,13 +21,15 @@ def index():
         group_by(Played.artist).\
         order_by(db.func.count(Played.artist).desc())
 
-    recent_songs = db.session.query(Played.artist, Played.artist_slug, Played.name, Played.station, Played.played_time).\
+    recent_songs = db.session.query(Played.artist, Played.artist_slug, Played.name, Played.song_slug, Played.station, Played.played_time).\
         order_by(Played.played_time.desc())[:15]
 
     t = request.args.get('t')
     start_date, end_date, span = parse_time(t)
-    if start_date:
-        stats = stats.filter(Played.played_time >= start_date, Played.played_time < end_date)
+    if not start_date:
+        start_date, end_date = find_start_date()
+
+    stats = stats.filter(Played.played_time >= start_date, Played.played_time < end_date)
 
     page = request.args.get('page', 1, type=int)
     stats = stats.paginate(page, 15, False)
@@ -51,7 +61,7 @@ def artist(name):
         group_by(Played.station).\
         order_by(db.func.count(Played.station).desc())
 
-    hourly_plays = db.session.query(db.func.strftime("%H", Played.played_time).lavel("hour"), db.func.count()).\
+    hourly_plays = db.session.query(db.func.strftime("%H", Played.played_time).label("hour"), db.func.count()).\
         filter(Played.artist_slug == name).\
         group_by("hour")
 
@@ -60,10 +70,12 @@ def artist(name):
         group_by("day")
 
     start_date, end_date, span = parse_time(request.args.get('t'))
-    if start_date:
-        song_stats = song_stats.filter(Played.played_time >= start_date, Played.played_time < end_date)
-        station_stats = station_stats.filter(Played.played_time >= start_date, Played.played_time < end_date)
-        hourly_plays = hourly_plays.filter(Played.played_time >= start_date, Played.played_time < end_date)
+    if not start_date:
+        start_date, end_date = find_start_date()
+
+    song_stats = song_stats.filter(Played.played_time >= start_date, Played.played_time < end_date)
+    station_stats = station_stats.filter(Played.played_time >= start_date, Played.played_time < end_date)
+    hourly_plays = hourly_plays.filter(Played.played_time >= start_date, Played.played_time < end_date)
 
     page = request.args.get('page', 1, type=int)
     song_stats = song_stats.paginate(page, 15, False)
@@ -110,19 +122,19 @@ def station(name):
 
     t = request.args.get('t')
     start_date, end_date, span = parse_time(t)
-    if start_date:
-        stats = stats.filter(Played.played_time >= start_date, Played.played_time < end_date)
-        total_plays = total_plays.filter(Played.played_time >= start_date, Played.played_time < end_date)
-        hourly_plays = hourly_plays.filter(Played.played_time >= start_date, Played.played_time < end_date)
-        daily_plays = daily_plays.filter(Played.played_time >= start_date, Played.played_time < end_date)
-        average_songs_per_day = average_songs_per_day.filter(Played.played_time >= start_date, Played.played_time < end_date)
+    if not start_date:
+        start_date, end_date = find_start_date()
 
-        total_song_length = total_song_length.filter(Played.played_time >= start_date, Played.played_time < end_date)
-        songs_length = total_song_length.first()[0]
-        total_seconds = end_date.timestamp() - start_date.timestamp()
-        song_percent = round(((songs_length / total_seconds) * 100), 2)
-    else:
-        song_percent = None
+    stats = stats.filter(Played.played_time >= start_date, Played.played_time < end_date)
+    total_plays = total_plays.filter(Played.played_time >= start_date, Played.played_time < end_date)
+    hourly_plays = hourly_plays.filter(Played.played_time >= start_date, Played.played_time < end_date)
+    daily_plays = daily_plays.filter(Played.played_time >= start_date, Played.played_time < end_date)
+    average_songs_per_day = average_songs_per_day.filter(Played.played_time >= start_date, Played.played_time < end_date)
+
+    total_song_length = total_song_length.filter(Played.played_time >= start_date, Played.played_time < end_date)
+    songs_length = total_song_length.first()[0]
+    total_seconds = end_date.timestamp() - start_date.timestamp()
+    song_percent = round(((songs_length / total_seconds) * 100), 2)
 
     songs_played = 0
 
@@ -171,4 +183,70 @@ def station(name):
             "song_percent": song_percent,
             "total": total_plays.first()[0]
         }
+    )
+
+
+@app.route('/search', methods=['GET'])
+def autocomplete():
+    search_term = request.args.get('term')
+    search_type = request.args.get('type')
+
+    songs = {
+        "results": [],
+        "pagination": {
+            "more": False
+        }
+    }
+
+    search_term = re.sub('[^0-9A-Za-z ]+', '', search_term)
+    if search_type == 'artist':
+        result = db.session.query(Played.artist).filter(Played.artist.like(f"%{search_term}%")).distinct().limit(10)
+    else:
+        result = db.session.query(Played.name, Played.artist).filter(Played.name.like(f"%{search_term}%")).distinct().limit(10)
+
+    for idx, entry in enumerate(result):
+        if search_type == 'artist':
+            text = f"{entry[0]}"
+        else:
+            text = f"{entry[0]} ({entry[1]})"
+
+        songs["results"].append({ "id": idx, "text": text })
+
+    return Response(json.dumps(songs), mimetype='application/json')
+
+
+@app.route("/song/<slug:name>")
+def song(name):
+    song = db.session.query(Played.name).filter(Played.song_slug == name).first()
+    artists = db.session.query(Played.artist, Played.artist_slug).filter(Played.song_slug == name).distinct()
+
+    station_stats = db.session.query(Played.station, db.func.count(Played.station)).\
+        filter(Played.song_slug == name).\
+        group_by(Played.station).\
+        order_by(db.func.count(Played.station).desc())
+
+    hourly_plays = db.session.query(db.func.strftime("%H", Played.played_time).label("hour"), db.func.count()).\
+        filter(Played.song_slug == name).\
+        group_by("hour")
+
+    daily_plays = db.session.query(db.func.strftime("%w", Played.played_time).label("day"), db.func.count()).\
+        filter(Played.song_slug == name).\
+        group_by("day")
+
+    start_date, end_date, span = parse_time(request.args.get('t'))
+    if not start_date:
+        start_date, end_date = find_start_date()
+
+    station_stats = station_stats.filter(Played.played_time >= start_date, Played.played_time < end_date)
+    hourly_plays = hourly_plays.filter(Played.played_time >= start_date, Played.played_time < end_date)
+    daily_plays = daily_plays.filter(Played.played_time >= start_date, Played.played_time < end_date)
+
+    return render_template(
+        'stats/song.html',
+        song=song[0],
+        artists=artists,
+        station_stats=station_stats,
+        span=span,
+        hourly_plays=hourly_plays,
+        daily_plays=daily_plays
     )
